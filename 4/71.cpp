a6856508de9abf10892ac602a768fe268453842f
Paul <petrfilatov2009@gmail.com>
2025-08-03 16:08:00 +0700

    Create 71.cpp

---
+++ 4/71.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+cin >> n;
+int max=1;
+for(int e=2;16>=e ;e++){
+if(n%(((1<<e)-1)*(1<<e-1))==0){
+max=(((1<<e)-1)*(1<<e-1));
+}
+}
+cout << max << endl;
+return 0;
+}
