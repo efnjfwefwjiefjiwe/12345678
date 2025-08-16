c04c74e343cf3ae47da3b8a4ff68c2e471a3776a
Paul <petrfilatov2009@gmail.com>
2025-08-16 16:53:57 +0700

    Create 88.cpp

---
+++ 5/88.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int gsd(int a,int b){
+while(b>0){
+a=a%b;
+swap(a,b);
+}
+return a;
+}
+int main(){
+ios::sync_with_stdio(0);
+cout.tie(0), cin.tie(0);
+int t;
+cin >> t;
+vector<int>ot;
+for(int e=0;t>e;e++){
+int a,b;
+cin >>a>>b;
+ot.push_back(a-gsd(a,b));
+}
+for(auto c:ot){
+cout <<c << endl;
+}
+return 0;    
+}
