314369f142c3e9a7df81c14b1483566e5c5b16f3
Paul <petrfilatov2009@gmail.com>
2025-08-16 20:57:27 +0700

    Create 25.cpp

---
+++ 1/25.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+long long int n ;
+int k ;
+cin >> n >> k;
+bool p = true;
+bool d = true;
+int cnt = 0 ;
+    while ( p ) {
+        if ( k > cnt ) {
+            if ( n % 200 == 0 ) {
+                n= n / 200 ;
+            }
+            else if ( cnt == k - 1 and n % 200 != 0 ) {
+                d = false ;
+                p = false ;
+            }
+            else {
+                cnt ++ ;
+                n = n * 5 + 1 ;
+            }
+        }
+        else {
+        p = false ;
+        }
+    cnt ++ ;
+    }
+if ( d ) {
+cout << n << endl ;
+}
+else{
+cout << n << "200" << endl ;
+}
+return 0 ;
+}
