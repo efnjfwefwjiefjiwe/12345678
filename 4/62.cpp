ae3a05a9a4c30a10d201c700386a937b4ad6b427
Paul <petrfilatov2009@gmail.com>
2025-08-17 15:02:21 +0700

    Update 62.cpp

--- 4/62.cpp
+++ 4/62.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-string a,b;
-cin >> a;
-cin >> b;
-if(a!=b){
-cout << "1" << endl;
-}
-else{
-cout << a << endl;
-}
-return 0;
+int main ( ) {
+  string a , b ;
+  cin >> a ;
+  cin >> b ;
+  if ( a != b ) {
+    cout << "1" << endl ;
+  }
+  else{
+    cout << a << endl ;
+  }
+  return 0 ;
 }
