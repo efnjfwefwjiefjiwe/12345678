fe82b91478ee58a684fe94fad89cacef75cd845c
Paul <petrfilatov2009@gmail.com>
2025-08-17 20:43:22 +0700

    Update 86.cpp

--- 5/86.cpp
+++ 5/86.cpp
@@ -1,23 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-int gsd(int &a,int &b){
-while(b>0){
-a=a%b;
-swap(a,b);
+int gsd(int & a, int & b) {
+  while (b > 0) {
+    a = a % b;
+    swap(a, b);
+  }
+  return a;
 }
-return a;
-}
-int main(){
-int t;
-cin >> t;
-vector<int>ot;
-for(int e=0;t>e;e++){
-int b,c;
-cin >> b>>c;
-ot.push_back(c/gsd(b,c));
-}
-for(auto c:ot){
-cout << c<< endl;
-}
-return 0;    
+int main ( ) {
+  int t;
+  cin >> t;
+  vector<int>ot;
+  for (int e = 0; t > e; e++) {
+    int b, c;
+    cin >> b >>c;
+    ot.push_back(c / gsd(b, c));
+  }
+  for (auto c : ot) {
+    cout << c<< endl;
+  }
+  return 0;    
 }
