ded63106783768ded108cb188be6981611ebbab1
Paul <petrfilatov2009@gmail.com>
2025-08-29 20:35:34 +0700

    Create 68.cpp

---
+++ 4/68.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+map<ll,int>prime;
+const ll ni =15 * 1e6 + 1;
+vector<ll>number(ni, 1e7);
+void resh(){
+    for(ll e = 2; ni > e; e++){
+        if(number[e]==1e7){
+            prime[e] = 0;
+            number[e] = e;
+            for(ll r = e * e; ni > r; r = r + e){
+                number[r] = min(number[r], e);
+            }
+        }
+    }
+}
+ll gsd(ll a, ll b){
+    while(b > 0){
+        a = a % b;
+        swap(a, b);
+    }
+    return a;
+}
+void solve(ll x){
+    while(x != 1){
+        prime[number[x]]++;
+        ll h = number[x];
+        while(x % h == 0){
+            x = x / h;
+        }
+    }
+}
+int main(){
+    resh();
+    int n;
+    cin >>n;
+    vector<ll>num(n);
+    ll u = 0;
+    for(int e = 0; n > e; e++){
+        cin >>num[e];
+        u = gsd(u, num[e]);
+    }
+    int cnt = 0;
+    for(int e = 0; n > e; e++){
+        num[e] = num[e] / u;
+        if(num[e] == 1){
+            ++cnt;
+        }
+    }
+    if(cnt == n){
+        cout << -1 << endl;
+    }
+    else{
+        for(int e = 0; n > e; e++){
+            solve(num[e]);
+        }
+        int maxi = 0;
+        for(auto c = prime.begin(); c != prime.end(); ++c ){
+            maxi = max(maxi, c->second);
+        }
+        cout <<n - maxi <<endl;
+    }
+    }
