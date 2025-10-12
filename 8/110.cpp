50229200bbf476c3c1161ed524a9a35aa7a35aa7
Paul <petrfilatov2009@gmail.com>
2025-10-12 16:45:49 +0700

    Create 110.cpp

---
+++ 8/110.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n, k;
+    cin >> n >> k;
+    int P[1001][2];
+    P[1][0] = k - 1;
+    P[1][1] = 0;
+    for (int i = 2; n >= i; ++i) {
+        P[i][1] = P[i - 1][0];
+        P[i][0] = (k - 1) * (P[i - 1][0] + P[i - 1][1]);
+    }
+    cout << P[n][0] + P[n][1] << endl;
+    return 0;
+}
